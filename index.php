4992ae58f19c114a9358d8e0d927d9cbbcde33c9
c1m3nk <whosme1226@gmail.com>
2025-09-16 11:46:10 +0700

    add

--- index.php
+++ index.php
@@ -5,7 +5,19 @@
 <body>
 <?php include "component/navbar.php"?>
 
-    <?php include "page/home.php"?>
+    <?php 
+    
+    $page = (isset($_GET['page'])) ? $_GET['page'] : "home";
+    if ($page == "home") {
+        include "page/home.php";
+    } elseif ($page == "detail") {
+        include "page/detail.php";
+    } elseif ($page == "shall") {
+        include "page/shall.php";
+    } else {
+        echo "<h1>404 Not Found</h1>";
+    }
+    ?>
 
 <?php include "component/footer.php"?>
 </body>
