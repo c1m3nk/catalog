faff6e0876dc03cd72ccba2716da83868cb4a411
c1m3nk <whosme1226@gmail.com>
2025-09-16 13:06:09 +0700

    add

---
+++ page/detail.php
@@ -0,0 +1,39 @@
+<div class="box">
+        <div class="product">
+            <img src="img/short.png" alt="Short Pants">
+
+            <div class="details">
+                <h3>short pants</h3>
+                <div class="price">Rp 55.000</div>
+
+                <div class="size-options">
+                    <span>size</span>
+                    <button>S</button>
+                    <button>M</button>
+                    <button>L</button>
+                    <button>XL</button>
+                </div>
+
+                <div class="quantity">
+                    <span>kuantitas</span>
+                    <button>-</button>
+                    <span>1</span>
+                    <button>+</button>
+                </div>
+
+                <div class="actions">
+                    <button>tambah ke keranjang</button>
+                    <button>beli sekarang</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="description">
+            <h2>deskripsi</h2>
+            <p>
+                Kaos basic dengan desain minimalis yang gampang dipaduin sama outfit apapun. Terbuat dari bahan katun
+                lembut dan adem, bikin nyaman dipakai seharian. Cocok buat nongkrong, hangout, atau sekadar gaya santai
+                di rumah. Pilihan warna netral bikin lo tetap stylish tanpa harus ribet.
+            </p>
+        </div>
+    </div>
---
+++ page/shall.php
@@ -0,0 +1,182 @@
+
+    <div class="container-shall">
+        <div class="grid">
+            <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+           
+            <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+            <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+            <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+            <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+             <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+           <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+           <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+           <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+             <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+            <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+            <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+            <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+            <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+           <a href="detail.html">
+                 <div class="card">
+                <span class="discount-tag">Upto 30%</span>
+                <img src="img/polo2.png" alt="Kaos Polo">
+                <div class="product-title">Kaos Polo</div>
+                <div>
+                    <span class="price">Rp. 127.000</span>
+                    <span class="old-price">Rp. 159.000</span>
+                </div>
+            </div>
+            </a>
+
+            <a href="detail.html">
+                <div class="card">
+                    <img src="img/polo1.png" alt="Kaos Polo">
+                    <div class="product-title">Kaos Polo</div>
+                    <div>
+                        <span class="price">Rp. 125.000</span>
+                    </div>
+                </div>
+            </a>
+
+
+
+        </div>
+    </div>
